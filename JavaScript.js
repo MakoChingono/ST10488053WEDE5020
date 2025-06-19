79d6b9007957ca8e97e8ba041eb61fc0bf002a62
MakoChingono <st10488053@rcconnect.edu.za>
2025-06-19 13:31:27 +0000

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,59 @@
+JavaScript
+function openTab(evt, tabNme) {
+    
+}
+//Set thre first tab as active by default
+document.addEventListener("DOMContentLoaded",function(){ document.querySelector(".tab").click();});
+
+//Simple product data
+const products =[
+{
+    title:"Elegant Bibles",
+    description: "Elegant, beautifully crafted Bibles designed to inspire your faith and add grace to every moment.",
+    images:"_images/c:\Users\RC_Student_lab\Downloads\elegant bible.jpg"
+   
+},
+{
+    title:"Children's Bibles",
+    description:"Colourful and easy to understand Bibles made to help children connect with God's word.",
+    images:"_images/c:\Users\RC_Student_lab\Downloads\kids bible.jpg"
+
+},
+
+{   title:"Leather Bibles",
+    description:"Durable and Stylish Bibles with soft leather covers, perfect for evrrday use or gifting.",
+    images:"_images/c:\Users\RC_Student_lab\Downloads\Leather bible.jpg"
+
+},
+
+{   title:"Large Print Bibles",
+    description:" Bibles with bigger, clearer text font for comfortable and easy reading.",
+    images:"_imagesc:\Users\RC_Student_lab\Downloads\large print.jp/"
+
+},
+
+
+];
+// load products on page load
+document.addEventListener("DOMContentLoaded", function() {
+    renderProducts(products);
+});
+
+// Rendering Function
+function renderProducts(productsToShow) {
+  }
+
+//Filter Function
+function filterProducts() {
+    const searchTerm = document.getElementById('productFilter').ariaValueMax.toLowerCase();
+    if (!searchTerm) {
+        renderProducts(products);
+        return;
+    }
+
+    //Show message if no results
+    if (filterProducts.length === 0) {
+        document.getElementById('productsContainer').innerHTML=
+        '<p.class="no-results">No.matching.products.found.</p>';
+    }
+}
